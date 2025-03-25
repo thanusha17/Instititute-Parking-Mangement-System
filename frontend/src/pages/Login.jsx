4d0055b1d812f29e1f60d52878f8e4418da9d12b
SaiChithra333 <saichithrarao@gmail.com>
2025-03-25 21:48:56 +0530

    Revert "edited login signup files"
    This reverts commit 31ec9b5ec3ed817e5de14fd29ee83def427f6018.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import backgroundImage from "../../assets/iiita_parking.png";
+import backgroundImage from "../assets/iiita_parking.png";
 
 const Login = () => {
   const [identifier, setIdentifier] = useState("");

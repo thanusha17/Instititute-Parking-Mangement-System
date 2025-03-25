08dd7dec8616e190794970c2a5908a6e13845e72
SaiChithra333 <saichithrarao@gmail.com>
2025-03-25 21:48:18 +0530

    Revert "added dbconnection ans dotenv file"
    This reverts commit cc81a149637355e8e0ee9460deef949bec0668dd.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/auth/Login";
-import Signup from "./pages/auth/Signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 
 function App() {
